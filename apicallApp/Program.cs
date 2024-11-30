ee9ba358b3709092ab3f6c28acb93b9ddcdf8eef
rosericazondekon <roseric_2000@yahoo.fr>
2024-11-29 21:51:04 -0500

    Add apicallApp

---
+++ apicallApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+var client = new HttpClient();
+
+Console.Write("Enter an API URL: ");
+string url = Console.ReadLine();
+
+if (url == null | url.Length == 0)
+{
+    Console.WriteLine("Setting to default URL: https://api.spacexdata.com/v3/capsules");
+    url = "https://api.spacexdata.com/v3/capsules";
+}
+
+
+try
+{
+    HttpResponseMessage responseMessage = await client.GetAsync(url);
+    responseMessage.EnsureSuccessStatusCode();
+
+    string responseBody = await responseMessage.Content.ReadAsStringAsync();
+    Console.WriteLine(responseBody);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Request error: {ex.Message}");
+}
